9df5633456aaff9ebc8d314f6f49b2b8cf311152
Andressa Bertolini <87765163+andressa-bertolini@users.noreply.github.com>
2026-04-08 21:57:09 -0300

    Delete tic-tac-toe directory

--- tic-tac-toe/src/components/RestartButton.js
+++
@@ -1,7 +0,0 @@
-const RestartButton = ({buttonClick}) => {
-    return(
-        <button className="restart-button" onClick={ buttonClick }>Restart</button>
-    )
-}
-
-export default RestartButton;
